93c41fc90248d4745f31019068ece4043cabb6a7
songtianlun <songtianlun@frytea.com>
2021-09-25 21:47:10 +0800

    commit 1.10

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,15 +6,31 @@ const Statistics = ({good, neutral, bad}) => {
       <h1>Statistics</h1>
       {good + neutral + bad > 0 && 
         <div>
-        <div>good: {good}</div>
-        <div>neutral: {neutral}</div>
-        <div>bad: {bad}</div>
-        <div>all: {good+neutral+bad}</div>
-        <div>average: {(good - bad) / (good + neutral + bad)}</div>
-        <div>postive: {(good + neutral) / (good + neutral + bad) * 100} %</div>
-      </div>
+          <div>good: {good}</div>
+          <div>neutral: {neutral}</div>
+          <div>bad: {bad}</div>
+          <div>all: {good+neutral+bad}</div>
+          <Average good={good} neutral={neutral} bad={bad} />
+          <div>postive: {(good + neutral) / (good + neutral + bad) * 100} %</div>
+        </div>
       || <div>no feedback given</div>
       }
+    </div>
+  )
+}
+
+const Average = ({good, neutral, bad}) => {
+  return (
+    <div>average: {(good - bad) / (good + neutral + bad)}</div>
+  )
+}
+
+const FeedBackButtons = ({setGood, setNeutral, setBad, good, neutral, bad}) => {
+  return (
+    <div>
+        <button onClick={() => setGood(good+1)}>good</button>
+        <button onClick={() => setNeutral(neutral+1)}>neutral</button>
+        <button onClick={() => setBad(bad+1)}>bad</button>
     </div>
   )
 }
@@ -28,9 +44,7 @@ const App = () => {
   return (
     <div>
       <h1>Give FeedBack</h1>
-      <button onClick={() => setGood(good+1)}>good</button>
-      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
-      <button onClick={() => setBad(bad+1)}>bad</button>
+      <FeedBackButtons setGood={setGood} setNeutral={setNeutral} setBad={setBad} good={good} neutral={neutral} bad={bad}/>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
