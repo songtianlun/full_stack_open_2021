4240f8e7540d2e6398212edf81a723f666c98c2b
songtianlun <songtianlun@frytea.com>
2021-09-25 21:14:02 +0800

    commit 1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,57 +1,14 @@
-import React from 'react'
-
-const Header = (props) => {
-  console.log(props)
-  return (<h1>{props.course}</h1>)
-}
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
-    </div>
-  )
-}
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.exercises}</p>
-  )
-}
+import React, { useState } from 'react'
 
 const App = () => {
-  const courses = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={courses.name} />
-      <Content 
-        parts={courses.parts}
-      ></Content>
-      <Total exercises={courses.parts[0].exercises + courses.parts[1].exercises + courses.parts[2].exercises}/>
+      code here
     </div>
   )
 }
