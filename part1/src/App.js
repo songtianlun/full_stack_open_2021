6b29d97642bb86b4c6a54a6da89e9207c8fec4dd
songtianlun <songtianlun@frytea.com>
2021-09-25 12:35:31 +0800

    commit 1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -27,29 +27,31 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const courses = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={courses.name} />
       <Content 
-        parts={parts}
+        parts={courses.parts}
       ></Content>
-      <Total exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Total exercises={courses.parts[0].exercises + courses.parts[1].exercises + courses.parts[2].exercises}/>
     </div>
   )
 }
