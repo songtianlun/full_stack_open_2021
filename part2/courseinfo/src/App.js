08e717954089345991541c6a5304568899bcab55
songtianlun <songtianlun@frytea.com>
2021-09-28 23:34:08 +0800

    commit 2.4 + 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -4,31 +4,55 @@ import Course from './components/Course'
 
 
 const App = () => {
-  const courses = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'Redux',
-        exercises: 11
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course courses={courses} />
+      <h1>Web development curriculum</h1>
+      {courses.map(course => (<Course key={course.id} courses={course} />))}
     </div>
   )
 }
--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Header = (props) => {
     // console.log(props)
-    return (<h1>{props.course}</h1>)
+    return (<h2>{props.course}</h2>)
   }
   const Content = (props) => {
     return (
       <div>
-        {props.parts.map(part => <Part key={part.name} part={part.name} exercises={part.exercises}/>)}
+        {props.parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises}/>)}
       </div>
     )
   }
@@ -34,11 +34,8 @@ const Course = ({courses}) => {
     return (
         <div>
             <Header course={courses.name} />
-            <Content 
-                parts={courses.parts}
-            >
-            </Content>
-            <Total parts={courses.parts} exercises={courses.parts[0].exercises + courses.parts[1].exercises + courses.parts[2].exercises}/>
+            <Content parts={courses.parts} />
+            <Total parts={courses.parts} />
         </div>
     )
 }
