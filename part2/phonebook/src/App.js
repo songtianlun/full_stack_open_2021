6b069e05e9ada6a98e7818a36374dcddc79683e7
songtianlun <songtianlun@frytea.com>
2021-10-04 14:24:04 +0800

    commit 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,20 +5,32 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log(event)
+    const newObj = {
+      name: newName
+    }
+    setPersons(persons.concat(newObj))
+    setNewName("")
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      {/* <div>debug: {newName}</div> */}
+      <form onSubmit={addNote}>
         <div>
-          name: <input />
+          name: <input onChange={(event)=>{setNewName(event.target.value)}} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => 
+          (<h3>{person.name}</h3>)
+        )}
     </div>
   )
 }
