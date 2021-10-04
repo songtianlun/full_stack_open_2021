effacbf672f2f3e133014df633d70dc88c9fd3be
songtianlun <songtianlun@frytea.com>
2021-10-04 18:49:21 +0800

    commit 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '+1(936)6662109'
-    }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [ searchPersons, setSearchPersons ] = useState(persons)
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+  useEffect(() => {
+    setSearchPersons(persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase())))
+  }, [filter])
   const addNote = (event) => {
     let hasPerson = false
     event.preventDefault()
@@ -24,7 +29,8 @@ const App = () => {
     } else if (!hasPerson){
       const newObj = {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: persons.length+1
       }
       setPersons(persons.concat(newObj))
       setNewName("")
@@ -38,6 +44,10 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       {/* <div>debug: {newName}</div> */}
+      <div>
+        filter show with <input onChange={(event) => (setFilter(event.target.value))} value={filter} />
+      </div>
+      <h2>Add New</h2>
       <form onSubmit={addNote}>
         <div>
           name: <input onChange={(event)=>{setNewName(event.target.value)}} value={newName} />
@@ -56,8 +66,8 @@ const App = () => {
           <tr><th>Name</th><th>Number</th></tr>
         </thead>
         <tbody>
-        {persons.map((person,index) => 
-          (<tr key={index}>
+        {searchPersons.map((person) => 
+          (<tr key={person.id}>
             <td>{person.name}</td>
             <td>{person.number}</td>
             </tr>)
