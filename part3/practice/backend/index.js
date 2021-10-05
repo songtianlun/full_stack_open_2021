375bcadfb17b6d2bb3f1fd60cec9e401878c292e
songtianlun <songtianlun@frytea.com>
2021-10-05 21:06:00 +0800

    Receiving data

--- part3/practice/backend/index.js
+++ part3/practice/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
       id: 1,
@@ -20,7 +22,14 @@ let notes = [
       date: "2019-05-30T19:20:14.298Z",
       important: true
     }
-  ]
+]
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+}
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
@@ -28,6 +37,26 @@ app.get('/', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
     res.json(notes)
+})
+
+app.post('/api/notes', (req, res) => {
+    const body = req.body
+    
+    if (!body.content) {
+        return res.status(400).json({ 
+          error: 'content missing' 
+        })
+    }
+    
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+    notes = notes.concat(note)
+    console.log(note)
+    res.json(note)
 })
 
 app.get('/api/notes/:id', (req, res) => {
