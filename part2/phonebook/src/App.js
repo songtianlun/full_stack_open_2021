e8ab4137185363e07c796559adeac0916175c2b6
songtianlun <songtianlun@frytea.com>
2021-10-05 15:12:05 +0800

    commit 2.15: Phonebook 步骤7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import phonebookServeice from './services/phonebook'
 
 const Filter = (props) => {
   return (
@@ -49,21 +49,16 @@ const Persons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [ searchPersons, setSearchPersons ] = useState(persons)
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then((response) => {
-        setPersons(response.data)
+    phonebookServeice
+      .getAll()
+      .then((phonebookdata) => {
+        setPersons(phonebookdata)
       })
   }, [])
   useEffect(() => {
@@ -87,9 +82,12 @@ const App = () => {
         number: newNumber,
         id: persons.length+1
       }
-      setPersons(persons.concat(newObj))
-      setNewName("")
-      setNewNumber("")
+      phonebookServeice.create(newObj).then(() => {
+        setPersons(persons.concat(newObj))
+        setNewName("")
+        setNewNumber("")
+      })
+      
     } else {
       window.alert(`${newName} is already added to phonebook`)
     }
