a6bde5ff25c2951ec43427de39f30e552e22de04
songtianlun <songtianlun@frytea.com>
2021-10-06 21:20:11 +0800

    Mongodb practice

--- part3/practice/backend/mongo.js
+++ part3/practice/backend/mongo.js
@@ -7,9 +7,20 @@ if ( process.argv.length<3 ) {
 
 const password = process.argv[2]
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.v3rtg.mongodb.net/fullstack-note-app?retryWrites=true&w=majority`
-mongoose.connect(url)
+const url = `mongodb+srv://fullstack:${password}@cluster0.v3rtg.mongodb.net/fullstack-note-app?retryWrites=true&w=majority`
+
+try {
+    mongoose.connect(url)
+} catch (error) {
+    console.log(error);
+}
+
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("fullstack-note-app").collection("notes");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 const noteSchema = new mongoose.Schema({
   content: String,
@@ -19,13 +30,25 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
+Note.createCollection();
+
 const note = new Note({
   content: 'HTML is Easy',
   date: new Date(),
   important: true,
 })
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// }).catch(err => {
+//     console.log(err)
+//     mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
